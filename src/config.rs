77a9eb0dc1fb94a491912d6121fe44d17ec646e4
jtong1231 <jtong5995@gmail.com>
2026-05-13 23:35:51 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -58,7 +58,7 @@ lazy_static::lazy_static! {
     static ref ONLINE: Mutex<HashMap<String, i64>> = Default::default();
     pub static ref PROD_RENDEZVOUS_SERVER: RwLock<String> = RwLock::new("".to_owned());
     pub static ref EXE_RENDEZVOUS_SERVER: RwLock<String> = Default::default();
-    pub static ref APP_NAME: RwLock<String> = RwLock::new("辉志云".to_owned());
+    pub static ref APP_NAME: RwLock<String> = RwLock::new("  ".to_owned());
     static ref KEY_PAIR: Mutex<Option<KeyPair>> = Default::default();
     static ref USER_DEFAULT_CONFIG: RwLock<(UserDefaultConfig, Instant)> = RwLock::new((UserDefaultConfig::load(), Instant::now()));
     pub static ref NEW_STORED_PEER_CONFIG: Mutex<HashSet<String>> = Default::default();
@@ -100,12 +100,12 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["cd3.meibrfewgfqgfdg4.top"];
-pub const RS_PUB_KEY: &str = "gRP1LZ3P4dMEatpfCA4qfNVTETTCmmn7cPkK+uX3HVk=";
-pub const RENDEZVOUS_PORT: i32 = 50696;
-pub const RELAY_PORT: i32 = 50697;
-pub const WS_RENDEZVOUS_PORT: i32 =50698;
-pub const WS_RELAY_PORT: i32 = 50699;
+pub const RENDEZVOUS_SERVERS: &[&str] = &["38.57.129.196"];
+pub const RS_PUB_KEY: &str = "Vstk4Hr0UOIG86luHpCqA16GhBpMc4vKbpX3qHF9wpU=";
+pub const RENDEZVOUS_PORT: i32 = 24386;
+pub const RELAY_PORT: i32 = 24387;
+pub const WS_RENDEZVOUS_PORT: i32 =24388;
+pub const WS_RELAY_PORT: i32 = 24389;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
@@ -1929,7 +1929,7 @@ impl UserDefaultConfig {
                 self.get_string(key, "balanced", vec!["best", "low", "custom"])
             }
             keys::OPTION_CODEC_PREFERENCE => {
-                self.get_string(key, "vp8", vec!["vp8", "vp9", "av1", "h264", "h265"])
+                self.get_string(key, "auto", vec!["vp8", "vp9", "av1", "h264", "h265"])
             }
             keys::OPTION_CUSTOM_IMAGE_QUALITY => self.get_num_string(key, 50.0, 10.0, 0xFFF as f64),
             keys::OPTION_CUSTOM_FPS => self.get_num_string(key, 30.0, 5.0, 120.0),
